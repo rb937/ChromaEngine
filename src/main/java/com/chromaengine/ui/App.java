876a9d1b2eaaa853fc54724d1dd617f8d4e85795
Rajat Bisht <rajatbisht913@gmail.com>
2026-06-16 13:56:55 +0530

    Added a logo

--- src/main/java/com/chromaengine/ui/App.java
+++ src/main/java/com/chromaengine/ui/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,6 +17,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("/com/chromaengine/primary.fxml"), 800, 600);
         stage.setTitle("ChromaEngine - Developer Build");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/com/chromaengine/icon.png")));
         stage.setScene(scene);
         stage.show();
     }
